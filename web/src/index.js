3e57bdff4b09d943fda7f165b28c2c3a52737e86
Matthew Brown <mattfdbrown@gmail.com>
2017-09-23 10:46:44 -0700

    add homepage

--- web/src/index.js
+++ web/src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import 'grommet/grommet-hpe.min.css';
+// import 'grommet/grommet.min.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
